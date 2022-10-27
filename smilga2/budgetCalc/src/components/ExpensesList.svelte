26cd605b3e1e0aef7bd2494e6d3b1558a599dbba
Dave Morgan <dave@dmorg.org>
2022-10-26 18:48:13 -0600

    strange happenings, git showing 1600+ changes

---
+++ smilga2/budgetCalc/src/components/ExpensesList.svelte
@@ -0,0 +1,18 @@
+<script>
+    import Title from "./Title.svelte";
+    import Expense from "./Expense.svelte";
+
+    export let expenses = [];
+</script>
+
+<section>
+    <Title title="expenses list" />
+    <ul>
+        {#each expenses as expense}
+            <!-- destructure expense to id, name, amount -->
+            <li><Expense {...expense} /></li>
+        {:else}
+            <h2>Currently, you have no expenses</h2>
+        {/each}
+    </ul>
+</section>
